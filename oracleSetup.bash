a65bc4be205ccb72a49fb212f69b6c059796453b
jrock <>
2007-11-10 00:11:32 +0000

    initial checking for oracleSetup.bash for lcls production env

---
+++ oracleSetup.bash
@@ -0,0 +1,19 @@
+#####################################################################
+#                                                                   #
+#  Title: oracleSetup.bash                                          #
+#                                                                   #
+#  Purpose: '.' this file to set your environment for Oracle        #
+#                                                                   #
+#  History:                                                         #
+#                                                                   #
+#  09nov2007 starting this initial version - NOT DONE YET, NOR IN   #
+#            IN CVS!                                                #
+#                                                                   #
+# oracle is in /usr/local/lcls/package/oracle                       #
+
+ export ORACLE_HOME=/usr/local/lcls/package/oracle/product/10.2.0/client_1
+ export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+ export PATH=$ORACLE_HOME/bin:$PATH
+ export TWO_TASK=MCCO
+
+
