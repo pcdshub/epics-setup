3ddc73ce5ab83de6de6d5eedc1f40fd79e8685d9
greg <>
2007-12-08 06:11:13 +0000

    Added ORACLE_PATH

--- oracleSetup.bash
+++ oracleSetup.bash
@@ -1,18 +1,19 @@
-#####################################################################
-#                                                                   #
-#  Title: oracleSetup.bash                                          #
-#                                                                   #
-#  Purpose: '.' this file to set your environment for Oracle        #
-#                                                                   #
-#  History:                                                         #
-#                                                                   #
-#  09nov2007 starting this initial version                          #
-#                                                                   #
-# oracle is in /usr/local/lcls/package/oracle                       #
-
- export ORACLE_HOME=/usr/local/lcls/package/oracle/product/10.2.0/client_1
- export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
- export PATH=$ORACLE_HOME/bin:$PATH
- export TWO_TASK=MCCO
-
-
+#-*-sh-*-
+#====================================================================
+#                                                
+#  Title: oracleSetup.bash                       
+#                                                           
+#  Purpose: '.' this file to set your environment for Oracle and 
+#           oracle clients.
+#        
+#--------------------------------------------------------------------
+#  History:
+#  $id$
+#  07-Dec-07 Greg Added ORACLE_PATH
+#  $log$ 
+#====================================================================
+export ORACLE_HOME=/usr/local/lcls/package/oracle/product/10.2.0/client_1
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$ORACLE_HOME/bin:$PATH
+export TWO_TASK=MCCO
+export ORACLE_PATH=/usr/local/lcls/tools/oracle
