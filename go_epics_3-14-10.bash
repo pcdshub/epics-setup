5572eafbc19216f2e172dc844d88c7672970be29
Ernest Williams <ernesto@slac.stanford.edu>
2009-01-13 08:48:09 +0000

    prepare easy way to switch releases. Just source these new files and you are in business :)

---
+++ go_epics_3-14-10.bash
@@ -0,0 +1,8 @@
+# Reset for EPICS R3.14.10
+# You just need to source this file and you
+# are ready for EPICS R3.14.10
+
+export EPICS_BASE_VER=base-R3-14-10
+export EPICS_EXTENSIONS_VER=R3-14-10
+source /usr/local/lcls/tools/script/ENVS.bash
+
---
+++ go_epics_3-14-8-2-lcls2-debug.bash
@@ -0,0 +1,8 @@
+# Reset for EPICS R3.14.8.2 lcls2 with Debug flags
+# You just need to source this file and you
+# are ready :)
+
+export EPICS_BASE_VER=base-R3-14-8-2-lcls2_Debug
+export EPICS_EXTENSIONS_VER=R3-14-8-2_Debug
+source /usr/local/lcls/tools/script/ENVS.bash
+
