a3f04e3b7ed03440a486fd202d77c01aa077999f
Ernest Williams <ernesto@slac.stanford.edu>
2009-01-14 01:33:42 +0000

    update file to understand production versus development; thanks Jingchen for the modifications

--- go_epics_3-14-10.bash
+++ go_epics_3-14-10.bash
@@ -2,7 +2,16 @@
 # You just need to source this file and you
 # are ready for EPICS R3.14.10
 
+# Define LCLS_ROOT, based on AFS for development or NFS for production
+if [ -d /afs/slac/g/lcls ]; then
+        export LCLS_ROOT=/afs/slac/g/lcls
+else
+        export LCLS_ROOT=/usr/local/lcls
+fi
+
+# setup for R3.14.10
+
 export EPICS_BASE_VER=base-R3-14-10
 export EPICS_EXTENSIONS_VER=R3-14-10
-source /usr/local/lcls/tools/script/ENVS.bash
+source ${LCLS_ROOT}/tools/script/ENVS.bash
 
--- go_epics_3-14-8-2-lcls2-debug.bash
+++ go_epics_3-14-8-2-lcls2-debug.bash
@@ -2,7 +2,16 @@
 # You just need to source this file and you
 # are ready :)
 
+# Define LCLS_ROOT, based on AFS for development or NFS for production
+if [ -d /afs/slac/g/lcls ]; then
+        export LCLS_ROOT=/afs/slac/g/lcls
+else
+        export LCLS_ROOT=/usr/local/lcls
+fi
+
+
 export EPICS_BASE_VER=base-R3-14-8-2-lcls2_Debug
 export EPICS_EXTENSIONS_VER=R3-14-8-2_Debug
-source /usr/local/lcls/tools/script/ENVS.bash
+
+source ${LCLS_ROOT}/tools/script/ENVS.bash
 
