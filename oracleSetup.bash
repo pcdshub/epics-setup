bd6d2e6ffaaadcce2d2bd68bc965c5fd861066ae
jrock <>
2009-07-23 22:58:55 +0000

    switched to 11.1.0 x

--- oracleSetup.bash
+++ oracleSetup.bash
@@ -9,14 +9,15 @@
 #--------------------------------------------------------------------
 #  History:
 #  $id$
+#  23-Jul-09 jrock switched to 11.1.0 client 
 #  23-Apr-09 jrock set TWO_TASK to SLACDEV for dev version
 #  07-Dec-07 Greg Added ORACLE_PATH
 #  $log$ 
 #====================================================================
-if [ -d /usr/local/lcls/package/oracle/product/10.2.0/client_1 ]; then
-   export ORACLE_HOME=/usr/local/lcls/package/oracle/product/10.2.0/client_1
-   export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
-   export PATH=$ORACLE_HOME/bin:$PATH
+if [ -d /usr/local/lcls/package/oracle/product/11.1.0/client_1 ]; then
+   export ORACLE_HOME=/usr/local/lcls/package/oracle/product/11.1.0/client_1
+   export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
+   export PATH=$ORACLE_HOME:$PATH
    export TWO_TASK=MCCO
    export ORACLE_PATH=/usr/local/lcls/tools/oracle
 else
