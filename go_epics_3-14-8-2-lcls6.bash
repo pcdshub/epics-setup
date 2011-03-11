4757b174044ba3636a99746f7fed4a6129cd436c
Ernest Williams <ernesto@slac.stanford.edu>
2011-03-11 23:46:29 +0000

    env for R3-14-8-2-lcls6

---
+++ go_epics_3-14-8-2-lcls6.bash
@@ -0,0 +1,17 @@
+# Reset for EPICS R3.14.8.2 lcls6 
+# You just need to source this file and you
+# are ready :)
+
+# Define LCLS_ROOT, based on AFS for development or NFS for production
+if [ -d /afs/slac/g/lcls ]; then
+        export LCLS_ROOT=/afs/slac/g/lcls
+else
+        export LCLS_ROOT=/usr/local/lcls
+fi
+
+
+export EPICS_BASE_VER=base-R3-14-8-2-lcls6
+export EPICS_EXTENSIONS_VER=R3-14-8-2
+
+source ${LCLS_ROOT}/tools/script/ENVS.bash
+
