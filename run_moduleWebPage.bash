6d96e6b7d9328e1fa1fb0f9e97a945ddc8cf590d
jrock <>
2011-07-22 17:11:09 +0000

    initial version

---
+++ run_moduleWebPage.bash
@@ -0,0 +1,22 @@
+#! /bin/bash 
+#
+# This is the calling script for moduleWebPage.pl for production lcls
+#
+#######################################################################
+# for now, moduleBuilder.pl is in the current working directory
+
+cd /usr/local/lcls/epics/setup
+
+source /usr/local/lcls/tools/script/ENVS.bash
+export RTEMS_VER=
+
+# cd to tmp so that the build order link in the web page points to "./"
+cd /tmp
+
+/usr/local/lcls/epics/setup/moduleWebPage.pl ./lcls_modules.html ./lcls_modules_order.txt GO
+
+#scp the files to web dir
+cp lcls_modules* /u1/lcls/tools/moduleWebPages/.
+
+#end of script
+
