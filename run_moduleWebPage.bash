43dc35cf4ef056a6b7d9365d3cc96da72e9681e9
jrock <>
2011-08-02 20:27:07 +0000

    added R3.14.12

--- run_moduleWebPage.bash
+++ run_moduleWebPage.bash
@@ -6,14 +6,18 @@
 # for now, moduleBuilder.pl is in the current working directory
 
 source /usr/local/lcls/tools/script/ENVS.bash
-cp /usr/local/lcls/epics/setup/moduleWebPageTop.html /tmp/.
 
 export RTEMS_VER=
+cp /usr/local/lcls/epics/setup/moduleWebPageTop.html /tmp/.
 
 # cd to tmp so that the build order link in the web page points to "./"
 cd /tmp
 
 /usr/local/lcls/epics/setup/moduleWebPage.pl ./lcls_modules.html ./lcls_modules_order.txt GO
+
+source /usr/local/lcls/epics/setup/go_epics_3-14-12.bash
+
+/usr/local/lcls/epics/setup/moduleWebPage.pl ./lcls_modules_12.html ./lcls_modules_order_12.txt GO
 
 #scp the files to web dir
 cp lcls_modules* /u1/lcls/tools/moduleWebPages/.
