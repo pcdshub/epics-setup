49a5773a42a70ca05d5e6e48e5510629f2c89ffb
Ernest Williams <ernesto@slac.stanford.edu>
2011-09-19 01:34:47 +0000

    setup env for EPICS R3-14-12 and RHEL6

---
+++ go_epics_3-14-12_swe.bash
@@ -0,0 +1,18 @@
+# Reset for EPICS R3.14.12
+# You just need to source this file and you
+# are ready :)
+
+# Define SWE_ROOT, based on AFS for development
+if [ -d /afs/slac/g/cd/swe/rhel6 ]; then
+        export SWE_ROOT=/afs/slac/g/cd/swe/rhel6
+fi
+
+
+export EPICS_BASE_VER=base-R3-14-12
+export EPICS_EXTENSIONS_VER=R3-14-12
+export EPICS_MODULES_VER=R3-14-12
+export EPICS_IOC_VER=R3-14-12
+
+source ${SWE_ROOT}/tools/script/ENVS_swe.bash
+
+
