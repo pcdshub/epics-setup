59b9183ce31f5067b037985da827becbfe56fb2a
niafong <>
2011-12-10 15:21:29 +0000

    export EPICS_MODULES_TOP after sourcing ENVS.bash so that EPICS_TOP is defined

--- go_epics_3-14-12.bash
+++ go_epics_3-14-12.bash
@@ -1,6 +1,14 @@
+# =========================================
 # Reset for EPICS R3.14.12
 # You just need to source this file and you
 # are ready :)
+#
+# -----------------------------------------
+# Changelog
+# 10-Dec-2011 Nia Fong
+#             export EPICS_MODULES_TOP after sourcing ENVS.bash to define EPICS_TOP
+# =========================================
+
 
 # Define LCLS_ROOT, based on AFS for development or NFS for production
 if [ -d /afs/slac/g/lcls ]; then
@@ -13,8 +21,8 @@ fi
 export EPICS_BASE_VER=base-R3-14-12
 export EPICS_EXTENSIONS_VER=R3-14-12
 export EPICS_MODULES_VER=R3-14-12
-export EPICS_MODULES_TOP=$EPICS_TOP/modules/${EPICS_MODULES_VER}
 
 source ${LCLS_ROOT}/tools/script/ENVS.bash
 
+export EPICS_MODULES_TOP=${EPICS_TOP}/modules/${EPICS_MODULES_VER}
 export TEST_STAND=/afs/slac/g/lcls/epics/TestStand
