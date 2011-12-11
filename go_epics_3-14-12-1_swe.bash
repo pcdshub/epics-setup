f6686b4ce6e6b9d31b07ed7533971c86b0909382
Ernest Williams <ernesto@slac.stanford.edu>
2011-12-11 22:16:02 +0000

    fixup for MODULES and Java

--- go_epics_3-14-12-1_swe.bash
+++ go_epics_3-14-12-1_swe.bash
@@ -11,7 +11,11 @@ fi
 export EPICS_BASE_VER=base-R3-14-12-1
 export EPICS_EXTENSIONS_VER=R3-14-12-1
 export EPICS_MODULES_VER=R3-14-12-1
+export EPICS_IOC_VER=R3-14-12
+
+# Use latest Java:
+export JAVA_VER=1.7.0_01
 
 source ${SWE_ROOT}/tools/script/ENVS_swe.bash
-
+export EPICS_MODULES_TOP=${EPICS_TOP}/modules/${EPICS_MODULES_VER}
 
--- go_epics_3-14-12-2_swe.bash
+++ go_epics_3-14-12-2_swe.bash
@@ -1,4 +1,4 @@
-# Reset for EPICS R3.14.12
+# Switch to EPICS R3.14.12.2
 # You just need to source this file and you
 # are ready :)
 
@@ -8,15 +8,14 @@ if [ -d /afs/slac/g/cd/swe/rhel5 ]; then
 fi
 
 
-export EPICS_BASE_VER=base-R3-14-12
-export EPICS_EXTENSIONS_VER=R3-14-12
-export EPICS_MODULES_VER=R3-14-12
-export EPICS_IOC_VER=R3-14-12
+export EPICS_BASE_VER=base-R3-14-12-2
+export EPICS_EXTENSIONS_VER=R3-14-12-2
+export EPICS_MODULES_VER=R3-14-12-2
+export EPICS_IOC_VER=R3-14-12-2
 
 # Use latest Java:
 export JAVA_VER=1.7.0_01
 
 source ${SWE_ROOT}/tools/script/ENVS_swe.bash
 export EPICS_MODULES_TOP=${EPICS_TOP}/modules/${EPICS_MODULES_VER}
-
 
