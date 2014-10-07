97a8f8d6f33fd2c6c2305fd91561ac17966ceade
Ernest Williams <ernesto@slac.stanford.edu>
2014-10-07 16:08:02 +0000

    set the correct HOST Architecture

--- epicsSetup_dev.bash
+++ epicsSetup_dev.bash
@@ -130,8 +130,9 @@ export EPICS_IOCS=$EPICS_TOP/iocCommon
 export EPICS_DATA=$LCLS_DATA/epics
 export EPICS_WWW=$WWW_ROOT/comp/unix/package/epics
 # temporary set EPICS_HOST_ARCH=linux-x86 during the transition to 64 bit
-#export EPICS_HOST_ARCH=`$EPICS_BASE_RELEASE/startup/EpicsHostArch`
-export EPICS_HOST_ARCH=linux-x86
+export EPICS_HOST_ARCH=`$EPICS_BASE_RELEASE/startup/EpicsHostArch`
+#export EPICS_HOST_ARCH=linux-x86
+
 #
 # For running IOCs and iocConsole
 #
