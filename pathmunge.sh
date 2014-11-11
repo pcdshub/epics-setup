5f3994490adabcf2d7a52a3749c346cfd3339639
Bruce Hill <bhill@slac.stanford.edu>
2014-11-11 01:00:22 +0000

    Added README_setup_dir and did a quick fix to lsb_release by running it w/o LD_LIBRARY_PATH
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@56 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

--- pathmunge.sh
+++ pathmunge.sh
@@ -7,7 +7,7 @@
 #
 # First, determine the release family
 # Use the -i option to get the distributor id
-LSB_DIST_ID=`lsb_release -i | /bin/cut -f2`
+LSB_DIST_ID=`LD_LIBRARY_PATH= lsb_release -i | /bin/cut -f2`
 
 # Check for rhel family: RedHatEnterpriseClient and RedHatEnterpriseServer
 LSB_FAMILY=`/bin/echo $LSB_DIST_ID | /bin/sed -e "s/RedHatEnterprise.*/rhel/"`
@@ -19,7 +19,7 @@ LSB_FAMILY=`/bin/echo $LSB_DIST_ID | /bin/sed -e "s/RedHatEnterprise.*/rhel/"`
 
 # Get the primary release number
 # For example, if "lsb_release -r" reports 5.8, our primary release is 5
-LSB_REL=`lsb_release -r | /bin/cut -f2 | /bin/cut -d. -f1`
+LSB_REL=`LD_LIBRARY_PATH= lsb_release -r | /bin/cut -f2 | /bin/cut -d. -f1`
 # Append the release number
 # For example, rhel5
 LSB_FAMILY=`/bin/echo ${LSB_FAMILY}${LSB_REL}`
