e7c77ecee793cb8421fac4e5d0a61c4368763589
Scott A. Stubbs <sstubbs@slac.stanford.edu>
2016-02-25 22:41:37 +0000

    Added some aliases, changed bigplcpc resolution
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@90 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

--- plcenv.sh
+++ plcenv.sh
@@ -10,7 +10,8 @@ XCSD='XCS=/reg/g/pcds/package/epics/3.14-dev/tools/current/scripts/iss/xcs_plc_d
 #regular size
 reg_plc_com="rdesktop -g 1200x800 -a 32 -x 0x80 -r disk:GEN=${PLC_DIR} -r disk:Package=/reg/common/package -r disk:$MECD -r disk:$CXID -r disk:PLC=$PLC_DIR"
 # big size
-big_plc_com="rdesktop -g 1700x900 -a 32 -x 0x80 -r disk:Package=/reg/common/package -r disk:${MECD} -r disk:${!CXID} -r disk:PLC=${PLC_DIR} -r disk:${XCSD}"
+#big_plc_com="rdesktop -g 1700x900 -a 32 -x 0x80 -r disk:Package=/reg/common/package -r disk:${MECD} -r disk:${!CXID} -r disk:PLC=${PLC_DIR} -r disk:${XCSD}"
+big_plc_com="rdesktop -g 1900x1028 -a 32 -x 0x80 -r disk:Package=/reg/common/package -r disk:${MECD} -r disk:${!CXID} -r disk:PLC=${PLC_DIR} -r disk:${XCSD}"
 # big size
 mega_plc_com="rdesktop -g 2100x1181 -a 32 -x 0x80 -r disk:Package=/reg/common/package -r disk:${MECD} -r disk:${!CXID} -r disk:PLC=${PLC_DIR} -r disk:${XCSD}"
 # small size
@@ -22,14 +23,17 @@ alias plcpc1='$reg_plc_com plc-prg-01 &'
 alias smallplcpc1='$small_plc_com plc-prg-01 &' 
 alias bigplcpc1='${big_plc_com} plc-prg-01 &' 
 alias megaplcpc1='${mega_plc_com} plc-prg-01 &' 
+alias tabletplcpc4='${tablet_plc_com} plc-prg-01 &'
 alias plcpc2='$reg_plc_com plc-prg-02 &' 
 alias smallplcpc2='$small_plc_com plc-prg-02 &' 
 alias bigplcpc2='${big_plc_com} plc-prg-02 &' 
 alias megaplcpc2='${mega_plc_com} plc-prg-02 &' 
+alias tabletplcpc2='${tablet_plc_com} plc-prg-02 &'
 alias plcpc3='$reg_plc_com plc-prg-03 &' 
 alias smallplcpc3='$small_plc_com plc-prg-03 &' 
 alias bigplcpc3='$big_plc_com plc-prg-03 &'
 alias megaplcpc3='${mega_plc_com} plc-prg-03 &' 
+alias tabletplcpc3='${tablet_plc_com} plc-prg-03 &'
 alias plcpc4='$reg_plc_com plc-prg-04 &' 
 alias smallplcpc4='$small_plc_com plc-prg-04 &' 
 alias bigplcpc4='$big_plc_com plc-prg-04 &'
