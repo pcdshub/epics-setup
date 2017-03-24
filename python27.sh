bd784397973b5623dafd4a36db81340f251d4736
Bruce Hill <bhill@slac.stanford.edu>
2017-03-24 02:56:45 +0000

    Updated to use python/2.7.5 from pkg_mgr
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@101 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

--- python27.sh
+++ python27.sh
@@ -4,16 +4,17 @@ if [ -z "$SETUP_SITE_TOP" ]; then
 	export SETUP_SITE_TOP=/reg/g/pcds/setup
 fi
 #if [ -z "$PSPKG_ROOT" ]; then
-export PSPKG_ROOT=/reg/common/package
+export PSPKG_ROOT=/reg/g/pcds/pkg_mgr
 #fi
+source $PSPKG_ROOT/etc/env_add_pkg.sh python/2.7.5
 source $SETUP_SITE_TOP/pathmunge.sh
 source $SETUP_SITE_TOP/lsb_family.sh
 
-if [ "$LSB_FAMILY" == "rhel6" ]; then
-	pythondir=$PSPKG_ROOT/python/2.7.2-rhel6
-else
-	pythondir=$PSPKG_ROOT/python/2.7.2-rhel5
-fi
-pathmunge $pythondir/bin
-ldpathmunge $pythondir/lib
+#if [ "$LSB_FAMILY" == "rhel6" ]; then
+#	pythondir=$PSPKG_ROOT/python/2.7.2-rhel6
+#else
+#	pythondir=$PSPKG_ROOT/python/2.7.2-rhel5
+#fi
+#pathmunge $pythondir/bin
+#ldpathmunge $pythondir/lib
 
