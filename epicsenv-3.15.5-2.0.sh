32fb9e7bc563950d896ccf832d238805f43b5c70
Bruce Hill <bhill@slac.stanford.edu>
2017-03-24 02:55:15 +0000

    Added an epics setup script EPICS base 3.15.5-2.0
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@100 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

---
+++ epicsenv-3.15.5-2.0.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+#
+# This script setup a working EPICS environment. It gives access
+# to EDM, VDCT and other development tools that are part of EPICS.
+#
+# Copyright @2012 SLAC National Accelerator Laboratory
+# Copyright @2017 SLAC National Accelerator Laboratory
+#
+# Note: You can pre-define EPICS_HOST_ARCH to select which host
+# architecture to use, or allow it to be auto-defined.
+# For PCDS, we support setting EPICS_HOST_ARCH to linux-x86
+# when running on a RHEL5 64 bit system, in order to run
+# the RHEL5 32 bit version of the EPICS applications
+
+# Setup the common directory env variables
+if [ -e /reg/g/pcds/pyps/config/common_dirs.sh ]; then
+	source /reg/g/pcds/pyps/config/common_dirs.sh
+else
+	source /afs/slac/g/pcds/config/common_dirs.sh
+fi
+# We're changing EPICS_SITE_TOP as of 3.15.5
+if [ -d /reg/g/pcds/epics ]; then
+	export EPICS_SITE_TOP=/reg/g/pcds/epics
+else
+	export EPICS_SITE_TOP=/afs/slac/g/pcds/epics
+fi
+
+# Setup the EPICS Channel Access environment
+source ${SETUP_SITE_TOP}/epics-ca-env.sh
+
+# get some functions for manipulating assorted env path variables
+source ${SETUP_SITE_TOP}/pathmunge.sh
+
+export EPICS_TOOLS_SITE_TOP=${EPICS_SITE_TOP}
+
+export BASE_MODULE_VERSION=R3.15.5-2.0
+export EPICS_BASE=${EPICS_SITE_TOP}/base/${BASE_MODULE_VERSION}
+export EPICS_MODULES_TOP=${EPICS_SITE_TOP}/${BASE_MODULE_VERSION}/modules
+
+export EPICS_EXTENSIONS=${EPICS_SITE_TOP}/extensions/R3.14.12
+
+if [ "$EPICS_HOST_ARCH" == "" ]; then
+	export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)
+fi
+
+# Default to linuxRT if startup/EpicsHostArch fails
+if [ "$EPICS_HOST_ARCH" == "" ]; then
+	export EPICS_HOST_ARCH=linuxRT-x86_64
+fi
+
+# If EPICS_HOST_ARCH is linux-x86_64 but not available, fall back to 32bit
+if [ ! -d ${EPICS_BASE}/bin/${EPICS_HOST_ARCH} ]; then
+	if [ "${EPICS_HOST_ARCH}" == "linux-x86_64" ]; then
+		# Try behaving as a 32bits system
+		echo "WARNING: No linux-x86_64 binaries.  Falling back to linux-x86"
+		EPICS_HOST_ARCH=linux-x86
+	fi
+fi
+if [ ! -d ${EPICS_BASE}/bin/${EPICS_HOST_ARCH} ]; then
+	echo "ERROR: No binaries in ${EPICS_BASE}/bin/${EPICS_HOST_ARCH}."
+fi
+
+# Set path to utilities provided by EPICS and its extensions
+pathmunge ${EPICS_BASE}/bin/${EPICS_HOST_ARCH}
+pathmunge ${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}
+export PATH
+
+# Set path to libraries provided by EPICS and its extensions (required by EPICS tools)
+ldpathmunge ${EPICS_BASE}/lib/${EPICS_HOST_ARCH}
+ldpathmunge ${EPICS_EXTENSIONS}/lib/${EPICS_HOST_ARCH}
+export LD_LIBRARY_PATH
+
+# The following setup is for EDM
+export EDMWEBBROWSER=mozilla
+export EDMCALC=${EPICS_EXTENSIONS}/svn_templates/edm/calc.list
+export EDMDATAFILES=.
+export EDMFILES=${EPICS_EXTENSIONS}/svn_templates/edm
+export EDMHELPFILES=${EPICS_EXTENSIONS}/helpFiles
+export EDMUSERLIB=${EPICS_EXTENSIONS}/lib/${EPICS_HOST_ARCH}
+export EDMOBJECTS=$EDMFILES
+export EDMPVOBJECTS=$EDMFILES
+export EDMFILTERS=$EDMFILES
+export EDMLIBS=$EDMUSERLIB
+
+# The following setup is for vdct
+# WARNING: java-1.6.0-sun must be installed on the machine running vdct!!!
+if [ -e ${EPICS_EXTENSIONS}/javalib/VisualDCT.jar ]; then
+	export VDCT_CLASSPATH="${EPICS_EXTENSIONS}/javalib/VisualDCT.jar"
+fi
+
+if [ -n "${BASH_VERSION}" ]; then
+	# Run PCDS bash shortcuts
+	source ${SETUP_SITE_TOP}/pcds_shortcuts.sh
+fi
+
