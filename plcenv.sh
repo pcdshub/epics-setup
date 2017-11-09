02b349591f6ca186564a5c7af04ed373513bca51
Scott A. Stubbs <sstubbs@slac.stanford.edu>
2017-11-09 22:24:43 +0000

    Added rdesktop commands back in as oldplcpc* and oldbigplcpc*
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@125 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

--- plcenv.sh
+++ plcenv.sh
@@ -1,5 +1,6 @@
 
 
+
 PLC_DIR='/reg/g/pcds/plc-common'
 
 MECD='MEC=/reg/g/pcds/package/epics/3.14-dev/tools/current/scripts/iss/mec_plc_dump'
@@ -8,7 +9,7 @@ XCSD='XCS=/reg/g/pcds/package/epics/3.14-dev/tools/current/scripts/iss/xcs_plc_d
 DRIVES=" -r disk:Package=/reg/common/package -r disk:${MECD} -r disk:${CXID} -r disk:PLC=${PLC_DIR} -r disk:${XCSD}"
 
 # command templates
-all='xfreerdp -sec-nla /d: /size:60% /drive:PLC,/reg/g/pcds/plc-common /v:'
+all='xfreerdp -sec-nla /d: /size:60% /drive:PLC,/reg/g/pcds/plc-common /cert-ignore /v:'
 allbig='xfreerdp -sec-nla /d: /size:90% /drive:PLC,/reg/g/pcds/plc-common /v:'
 
 alias plcpc1="${all}plc-prg-01 &" 
@@ -19,3 +20,19 @@ alias plcpc3="${all}plc-prg-03 &"
 alias bigplcpc3="${allbig}plc-prg-03 &"
 alias plcpc4="${all}plc-prg-04 &"
 alias bigplcpc4="${allbig}plc-prg-04 &"
+
+# old rdesktop command templates - as a fallback
+#regular size
+reg_plc_com="rdesktop -g 1200x800 -a 32 -x 0x80 ${DRIVES}"
+# big size
+big_plc_com="rdesktop -g 1800x1028 -a 32 -x 0x80 ${DRIVES}"
+
+alias oldplcpc1='$reg_plc_com plc-prg-01 &' 
+alias oldbigplcpc1='${big_plc_com} plc-prg-01 &' 
+alias oldplcpc2='$reg_plc_com plc-prg-02 &' 
+alias oldbigplcpc2='${big_plc_com} plc-prg-02 &' 
+alias oldplcpc3='$reg_plc_com plc-prg-03 &' 
+alias oldbigplcpc3='$big_plc_com plc-prg-03 &'
+alias oldplcpc4='$reg_plc_com plc-prg-04 &' 
+alias oldbigplcpc4='$big_plc_com plc-prg-04 &'
+
