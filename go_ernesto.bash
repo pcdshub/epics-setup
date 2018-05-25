264901cfba73370effef30dc52f4ae6c6b819622
Bruce Hill <bhill@slac.stanford.edu>
2018-05-25 15:12:06 -0700

    Adding a few more go_*.bash scripts to CVS

---
+++ go_ernesto.bash
@@ -0,0 +1,12 @@
+# =========================================
+# Reset for EPICS R3-15
+# You just need to source this file and you
+# are ready :)
+# =========================================
+
+source $EPICS_SETUP/go_epics_3.15.5-1.0.bash
+source /afs/slac/g/pcds/config/common_dirs.sh
+source /afs/slac/g/lcls/tools/script/release/eco_tools/R1.7/add-to-env.sh
+
+
+
