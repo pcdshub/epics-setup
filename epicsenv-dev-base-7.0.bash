ee248b7ef1043df18264575f78116c5b6afa94a9
Bruce Hill <bhill@slac.stanford.edu>
2018-07-10 23:16:23 -0700

    Simplify epicsenv-* scripts

--- epicsenv-dev-base-7.0.bash
+++ epicsenv-dev-base-7.0.bash
@@ -12,7 +12,7 @@ source /afs/slac/g/lcls/epics/config/common_dirs.sh
 
 # Select the EPICS base version and EPICS extensions version
 export BASE_MODULE_VERSION=dev-base-7.0
-export EPICS_EXTENSIONS=${EPICS_SITE_TOP}/extensions/extensions-R3.15.5
+export EPICS_EXTENSIONS=${EPICS_SITE_TOP}/extensions/R0.2.0
 
 export EPICS_BASE=${EPICS_SITE_TOP}/base/${BASE_MODULE_VERSION}
 export EPICS_MODULES=${EPICS_SITE_TOP}/${BASE_MODULE_VERSION}/modules
@@ -23,7 +23,7 @@ export EPICS_MODULES_VER=${BASE_MODULE_VERSION}
 export EPICS_BASE_RELEASE=${EPICS_BASE}
 export EPICS_MODULES_TOP=${EPICS_MODULES}
 
-unset PVACCESS PVDATA PVDATABASE NORMATIVETYPES
+unset PVACCESSCPP
 
 source ${SETUP_SITE_TOP}/generic-epics-setup.bash
 
