a225714f8222dea0a5c9d0c89571ffe41c9590b0
Bruce Hill <bhill@slac.stanford.edu>
2018-10-25 22:30:49 +0000

    Set EDMFONTFILE=/reg/g/pcds/epics/extensions/R3.14.12/svn_templates/edm/fonts.list in edmPath_lclsMap.sh
    git-svn-id: file:///afs/slac/g/pcds/svn/netconfig/setup@143 ac5f43dc-fe9c-476a-8fd5-0757c593c5f9

--- edmPath_lclsMap.sh
+++ edmPath_lclsMap.sh
@@ -3,6 +3,7 @@ source /reg/g/pcds/setup/pathmunge.sh
 # Setup EDMDATAFILES path for LCLS Maps
 export EDM=/reg/g/pcds/package/epics/lcls/tools/edm/display
 export EDMFILES=/reg/g/pcds/package/epics/lcls/tools/edm/config
+export EDMFONTFILE=/reg/g/pcds/epics/extensions/R3.14.12/svn_templates/edm/fonts.list
 
 export EDMDATAFILES=
 edmpathmunge $EDM/mps
