4f2d05bf214cca0836eff873b01a956d68e7810f
Matt Gibbs <mgibbs@slac.stanford.edu>
2019-10-01 11:37:32 -0700

    Fix missing address in EPICS_CA_ADDR_LIST.  This was causing LCLS-II PVs to be un-reachable by caget.

--- envSet_prod.bash
+++ envSet_prod.bash
@@ -10,6 +10,8 @@
 #
 #--------------------------------------------------------------
 #  Mod:
+#    01-Oct-2019, M. Gibbs (mgibbs)
+#   Fixed missing item in EPICS_CA_ADDR_LIST needed for LCLS-II PVs
 #    10-Jul-2018, B. Hill (bhill)
 #	Created new env script for accessing production PVs from production hosts
 #
@@ -21,7 +23,7 @@ export EPICS_PVA_ADDR_LIST="mccas0.slac.stanford.edu"
 export EPICS_PVA_BROADCAST_PORT=5056
 export EPICS_PVA_AUTO_ADDR_LIST=FALSE
 export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_ADDR_LIST="172.27.3.255:5068 mcc-dmz 172.21.40.63:5064 172.27.72.24:5070"
+export EPICS_CA_ADDR_LIST="172.27.3.255:5068 mcc-dmz 172.21.40.63:5064 172.27.72.24:5070 172.27.131.255:5068"
 export EPICS_CA_REPEATER_PORT="5069"
 export EPICS_CA_SERVER_PORT="5068"
 export EPICS_TS_NTP_INET="134.79.151.11"
