ad7f469f85e4139183a3473dc607c0358081468a
Bruce Hill <bhill@slac.stanford.edu>
2021-07-02 18:20:40 -0700

    Use edmPath_lclsHome.sh to set EDMDATAFILES same as PROD.

--- pydm_lclshome.sh
+++ pydm_lclshome.sh
@@ -5,15 +5,10 @@ shopt -s extglob
 source /reg/g/pcds/pyps/conda/dev_conda
 LCLS_TOOLS=/reg/g/pcds/package/epics/lcls/tools
 
-export EDMDATAFILES=
-for dir in $PACKAGE_SITE_TOP/epics/lcls/tools/edm/display/!(CVS|*old*)
-do
-	if [ -d "$dir" ]; then
-		export EDMDATAFILES=${dir}:${EDMDATAFILES}
-	fi
-done
-
-export EDMDATAFILES=.:..:${EDMDATAFILES}
+if [ -z "$SETUP_SITE_TOP" ]; then
+	SETUP_SITE_TOP=/reg/g/pcds/setup
+fi
+source $SETUP_SITE_TOP/edmPath_lclsHome.sh
 
 PYDM_DISPLAYS_PATH=$LCLS_TOOLS/pydm/display/:$PYDM_DISPLAYS_PATH PYDM_DEFAULT_PROTOCOL=ca pydm --stylesheet $LCLS_TOOLS/pydm/stylesheet/default.qss $LCLS_TOOLS/pydm/display/lcls/lclshome/lclshome.py ${@}
 
