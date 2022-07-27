232e0394979b4d13f6f71ffeaa5fd82ef85868ae
Zachary Lentz <zlentz@slac.stanford.edu>
2022-07-27 11:35:54 -0700

    Include the new --no-web arg for pmps launcher. Web support still crashes operator consoles.

--- pmps-launcher.sh
+++ pmps-launcher.sh
@@ -5,4 +5,4 @@ source /cds/group/pcds/pyps/conda/pcds_conda
 
 pushd "/cds/group/pcds/epics-dev/screens/pydm/pmps-ui"
 
-pydm --hide-nav-bar --hide-status-bar -m "CFG=$1" pmps.py &
+pydm --hide-nav-bar --hide-status-bar -m "CFG=$1" pmps.py --no-web &
