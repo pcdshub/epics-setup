3eb2678d4484b6b88634fb8fc8396f3b36315d47
Zachary Lentz <zlentz@slac.stanford.edu>
2025-05-12 11:42:37 -0700

    MNT: update conda ver, new path fixes splash loading somehow

--- pmps-launcher.sh
+++ pmps-launcher.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-export PCDS_CONDA_VER=5.8.0
+export PCDS_CONDA_VER=5.9.1
 source /cds/group/pcds/pyps/conda/pcds_conda
 
-pushd "/cds/group/pcds/epics-dev/screens/pydm/pmps-ui"
-
-pydm --hide-nav-bar --hide-status-bar -m "CFG=$1" pmps.py --no-web &
+cd "/cds/group/pcds/epics-dev/screens/pydm/pmps-ui"
+pydm --hide-nav-bar --hide-status-bar -m "CFG=$1" pmpsui/pmps.py --no-web &
