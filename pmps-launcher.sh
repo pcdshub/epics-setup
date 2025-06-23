8e02b480f3379695aa16d34f1407d526d4448a97
Zachary Lentz <zlentz@slac.stanford.edu>
2025-06-23 15:27:19 -0700

    FIX: pmps launch new invocation forgot to commit last month

--- pmps-launcher.sh
+++ pmps-launcher.sh
@@ -4,4 +4,4 @@ export PCDS_CONDA_VER=5.9.1
 source /cds/group/pcds/pyps/conda/pcds_conda
 
 cd "/cds/group/pcds/epics-dev/screens/pydm/pmps-ui"
-pydm --hide-nav-bar --hide-status-bar -m "CFG=$1" pmpsui/pmps.py --no-web &
+python -m pmpsui --area "$1" --no-web &
