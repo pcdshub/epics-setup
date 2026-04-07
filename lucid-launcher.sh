dea97879def97319fdfb6d37cdfb29f20f2d595a
Zachary Lentz <zlentz@slac.stanford.edu>
2026-04-07 13:31:49 -0700

    MNT: force TMO and UED to use old lucid for now

--- lucid-launcher.sh
+++ lucid-launcher.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-source /reg/g/pcds/pyps/conda/dev_conda
+# shellcheck disable=SC1091
+source /cds/group/pcds/pyps/conda/dev_conda
 
-lucid "$1" --toolbar=$LUCID_CONFIG/"$1"_toolbar.yaml &
+case "$1" in
+  TMO|UED)
+    # Hold off on the update for now
+    export PYTHONPATH="/cds/group/pcds/pyps/apps/dev/lucid-old:$PYTHONPATH"
+    ;;
+  *)
+    # All others: keep the dev_conda paths
+    ;;
+esac
+
+lucid "$1" --toolbar="$LUCID_CONFIG"/"$1"_toolbar.yaml &
