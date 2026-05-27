94e7e350a86430ea7690e5c713e87ced52807012
Zachary Lentz <zlentz@slac.stanford.edu>
2026-05-26 18:05:32 -0700

    WIP: lucid-launcher.sh with dev option

--- lucid-launcher.sh
+++ lucid-launcher.sh
@@ -4,7 +4,7 @@
 source /cds/group/pcds/pyps/conda/dev_conda
 
 case "$1" in
-  TMO|UED)
+  UED)
     # Hold off on the update for now
     export PYTHONPATH="/cds/group/pcds/pyps/apps/dev/lucid-old:$PYTHONPATH"
     ;;
@@ -12,5 +12,10 @@ case "$1" in
     # All others: keep the dev_conda paths
     ;;
 esac
+
+if [ "$2" == "dev" ]; then
+  echo "Loading lucid-stale-fix dev build"
+  export PYTHONPATH="/cds/group/pcds/pyps/apps/dev/lucid-stale-fix:$PYTHONPATH"
+fi
 
 lucid "$1" --toolbar="$LUCID_CONFIG"/"$1"_toolbar.yaml &
